c7440b2bd5d712e372571e1a1157436f7aca1eac
tanjoresriram <tanjore@gmail.com>
2025-06-03 10:48:06 -0400

    changes made for tsx

--- deployfile/src/App.tsx
+++ deployfile/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+ import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+         <button onClick={fetchFile}> Download file </button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +23,28 @@ function App() {
       </header>
     </div>
   );
+}
+function fetchFile() {
+  axios({
+        url: `https://github.com/tanjoresriram/contentps/blob/main/myps.ps1`,
+        method: "GET",
+         responseType: "blob" // important
+    }).then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute(
+            "download",
+            `myps.ps1`
+        );
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up and remove the link
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+    });
+  throw new Error('Function not implemented.');
 }
 
 export default App;
