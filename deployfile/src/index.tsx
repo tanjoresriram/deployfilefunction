6f3eda179557c2a16ab5fe09ac817ce60a39bb15
tanjoresriram <tanjore@gmail.com>
2025-06-03 10:14:53 -0400

    Made changes in the index

--- deployfile/src/index.tsx
+++ deployfile/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+ import axios from 'axios';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <button onClick={fetchFile}> Download file </button>
   </React.StrictMode>
 );
 
@@ -17,3 +18,29 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+function fetchFile() {
+  axios({
+        url: `https://github.com/tanjoresriram/contentps/blob/main/myps.ps1`,
+        method: "GET",
+         responseType: "blob" // important
+    }).then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute(
+            "download",
+            `myps.ps1`
+        );
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up and remove the link
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+    });
+  throw new Error('Function not implemented.');
+}
+
+
+
